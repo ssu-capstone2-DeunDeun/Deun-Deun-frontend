60e7fc97a219d2bd995215761e541408c236a597
Suyoung <jsy19970512@naver.com>
2021-04-13 18:49:17 +0900

    add test button

--- deundeun/src/pages/MyPage/index.jsx
+++ deundeun/src/pages/MyPage/index.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import { Header, Container } from 'pages/MyPage/styles';
+import React, { useCallback } from 'react';
+import { Header, Container, AddQuestionButton, DeleteQuestionButton, ButtonContainer } from 'pages/MyPage/styles';
+
 const MyPage = () => {
+	const onClickAddQuestion = useCallback(() => {
+		console.log('add question');
+	}, []);
+
+	const onClickDeleteQuestion = useCallback(() => {
+		console.log('delete question');
+	}, []);
+
 	return (
 		//
 		<Container>
-			<Header>Header</Header>
+			<Header>지원서 양식 생성</Header>
+			<ButtonContainer>
+				<AddQuestionButton onClick={onClickAddQuestion}>+</AddQuestionButton>
+				<DeleteQuestionButton onClick={onClickDeleteQuestion}>-</DeleteQuestionButton>
+			</ButtonContainer>
 		</Container>
 	);
 };
--- deundeun/src/pages/MyPage/styles.js
+++ deundeun/src/pages/MyPage/styles.js
@@ -11,9 +11,25 @@ export const Header = styled.header`
 	height: 64px;
 	display: flex;
 	width: 100%;
-	--saf-0: rgba(var(--sk_foreground_low, 29, 28, 29), 0.13);
 	box-shadow: 0 1px 0 var(--saf-0);
-	padding: 20px 16px 20px 20px;
+	margin-left: 1em;
 	font-weight: bold;
 	align-items: center;
+`;
+
+export const ButtonContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+`;
+
+export const AddQuestionButton = styled.button`
+	width: 30px;
+	height: 30px;
+	margin-right: 3em;
+	margin-left: 1em;
+`;
+
+export const DeleteQuestionButton = styled.button`
+	width: 30px;
+	height: 30px;
 `;
