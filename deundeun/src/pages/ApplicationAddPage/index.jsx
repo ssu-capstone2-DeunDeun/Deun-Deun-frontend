c8753c21e7a50670682ca3099eb589a9a70aa29c
Suyoung <jsy19970512@naver.com>
2021-05-05 22:52:38 +0900

    feat: 관리자 페이지 - 동아리 지원서 양식 상세 페이지 구현 Resolves: #12

--- deundeun/src/pages/ApplicationAddPage/index.jsx
+++ deundeun/src/pages/ApplicationAddPage/index.jsx
@@ -1,13 +1,89 @@
-import React, { useEffect } from 'react';
-
+import DropdownMenu from 'components/DropdownMenu';
+import { TitleKorean } from 'pages/RecruitAddPage/styles';
+import React, { useEffect, useState } from 'react';
+import { ContainerColumn, ContainerPage, ContainerRow } from 'styles';
+import {
+	AddQuestionButton,
+	AddSelectionButton,
+	AnswerNumber,
+	ApplicationTitleInput,
+	DropdownContainer,
+	InnerContainer,
+	QuestionInput,
+	QuestionNumber
+} from './styles';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 const ApplicationAddPage = ({ setAddNewForm }) => {
+	const [questionTypeIndex, setQuestionTypeIndex] = useState(0);
+	const [answerTypeIndex, setAnswerTypeIndex] = useState(0);
+
 	useEffect(() => {
 		return () => setAddNewForm(false);
 	}, []);
 
+	let questionTypes = ['주관식', '선다형'];
+	let answerTypes = ['단일 선택', '복수 선택'];
+
 	return (
 		//
-		<h1>ApplicationAddPage</h1>
+		<ContainerPage style={{ width: '68vw', minHeight: '150vh' }}>
+			<ContainerColumn>
+				<TitleKorean>지원서 제목</TitleKorean>
+				<ApplicationTitleInput placeholder="제목을 입력해주세요."></ApplicationTitleInput>
+			</ContainerColumn>
+			<ContainerColumn>
+				<TitleKorean style={{ marginBottom: '1em' }}>질문</TitleKorean>
+				<ContainerRow>
+					<QuestionNumber>Q1</QuestionNumber>
+					<DropdownContainer>
+						{`${questionTypes[questionTypeIndex]}`}
+						<DropdownMenu
+							options={questionTypes}
+							selectedIndex={questionTypeIndex}
+							setSelectedIndex={setQuestionTypeIndex}
+						></DropdownMenu>
+					</DropdownContainer>
+					<QuestionInput placeholder="질문을 입력해주세요." />
+				</ContainerRow>
+				<ContainerRow>
+					<QuestionNumber>Q2</QuestionNumber>
+					<DropdownContainer>
+						{`${questionTypes[questionTypeIndex]}`}
+						<DropdownMenu
+							options={questionTypes}
+							selectedIndex={questionTypeIndex}
+							setSelectedIndex={setQuestionTypeIndex}
+						></DropdownMenu>
+					</DropdownContainer>
+					<DropdownContainer>
+						{`${answerTypes[answerTypeIndex]}`}
+						<DropdownMenu
+							options={answerTypes}
+							selectedIndex={answerTypeIndex}
+							setSelectedIndex={setAnswerTypeIndex}
+						></DropdownMenu>
+					</DropdownContainer>
+					<QuestionInput className="select-title" placeholder="질문을 입력해주세요." />
+				</ContainerRow>
+
+				<ContainerRow>
+					<AnswerNumber>A2</AnswerNumber>
+					<ContainerColumn>
+						<QuestionInput placeholder="선택지를 입력해주세요." />
+						<QuestionInput placeholder="선택지를 입력해주세요." />
+						<QuestionInput placeholder="선택지를 입력해주세요." />
+						<QuestionInput placeholder="선택지를 입력해주세요." />
+						<AddSelectionButton>+ 선택지 추가하기</AddSelectionButton>
+					</ContainerColumn>
+				</ContainerRow>
+				<AddQuestionButton>
+					<InnerContainer>
+						<AddCircleOutlineIcon style={{ marginRight: '0.5em' }} />
+						질문 추가하기
+					</InnerContainer>
+				</AddQuestionButton>
+			</ContainerColumn>
+		</ContainerPage>
 	);
 };
 
--- deundeun/src/pages/ApplicationAddPage/styles.js
+++ deundeun/src/pages/ApplicationAddPage/styles.js
@@ -1,1 +1,97 @@
 import styled from '@emotion/styled';
+
+export const ApplicationTitleInput = styled.input`
+	border: none;
+	background-color: #f7f7f7;
+	height: 6vh;
+	padding-left: 1em;
+	margin-top: 1em;
+	margin-bottom: 3em;
+`;
+
+export const QuestionNumber = styled.div`
+	width: 4.02vw;
+	height: 5.8vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-family: 'NotoSansKR';
+	font-weight: 900;
+	font-size: 1rem;
+	background-color: #f7f7f7;
+	margin-bottom: 1.3em;
+	margin-right: 0.3em;
+`;
+
+export const DropdownContainer = styled.div`
+	width: 10vw;
+	height: 5.8vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-family: 'NotoSansKR';
+	font-weight: 900;
+	font-size: 1rem;
+	background-color: #f7f7f7;
+	margin-right: 0.3em;
+`;
+
+export const AnswerNumber = styled.div`
+	width: 4.02vw;
+	height: 5.47vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-family: 'NotoSansKR';
+	font-weight: 900;
+	font-size: 1rem;
+	background-color: #f7f7f7;
+	margin-right: 0.3em;
+`;
+
+export const QuestionInput = styled.input`
+	padding-left: 1em;
+	background-color: #f7f7f7;
+	font-size: 1rem;
+	border: none;
+	height: 5.47vh;
+	width: 40vw;
+	margin-bottom: 0.6em;
+	width: calc(68vw - 4.02vw);
+	&.select-title {
+		width: calc(68vw - 4.02vw - 10vw);
+	}
+`;
+
+export const AddSelectionButton = styled.div`
+	cursor: pointer;
+	display: flex;
+	height: 5.47vh;
+	padding-left: 1em;
+	align-items: center;
+	background-color: #f7f7f7;
+	color: #606060;
+`;
+
+export const AddQuestionButton = styled.div`
+	margin-top: 5em;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-bottom: 10em;
+	height: 20vh;
+`;
+
+export const InnerContainer = styled.div`
+	display: flex;
+	cursor: pointer;
+	align-items: center;
+	justify-content: center;
+	width: 15%;
+	height: 30%;
+	&:hover {
+		background-color: #a1a1a1;
+		color: #fff;
+	}
+	border-radius: 1em;
+`;
