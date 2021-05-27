49d3d8fe80bad8a369fa52b11812a59c980a1059
sanglok <evertree6031@gmail.com>
2021-05-27 15:49:01 +0900

    Feature : make loginUserInfo state resolved:#28

---
+++ deundeun/src/modules/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "../../node_modules/redux/";
+import loginUserInfo from "./loginUserInfo";
+
+const rootReducer = combineReducers({
+    loginUserInfo,
+})
+
+
+export default rootReducer;
--- deundeun/src/modules/loginUserInfo.js
+++ deundeun/src/modules/loginUserInfo.js
@@ -1,11 +1,25 @@
-// const INPUT_LOGIN_USER_INFO = "loginUserInfo/INPUT_LOGIN_USER_INFO";
+import { createAction, handleActions } from "redux-actions";
 
+const INPUT_LOGIN_USER_INFO = "loginUserInfo/INPUT_LOGIN_USER_INFO";
 
+const inputLoginUserInfo = createAction(INPUT_LOGIN_USER_INFO);
 
-// const initialState = {
-//     nickname: "",
-//     name: "",
-//     email: "",
-//     userImageUrl: "",
-//     hashtag: [],
-// };
+const initialState = {
+    nickname: "",
+    name: "",
+    email: "",
+    userImageUrl: "",
+    hashtag: [],
+};
+
+const loginUserInfo = handleActions(
+    {
+        [inputLoginUserInfo]: (state, { payload: type, value }) => ({
+            ...state,
+            type: value,
+        }),
+    },
+    initialState
+);
+
+export default loginUserInfo;
